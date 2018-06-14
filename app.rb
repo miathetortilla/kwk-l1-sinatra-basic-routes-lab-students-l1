5e5aa290e0eeb0e60407b7f4152c4b861de8bd4a
miathetortilla <mia.lamontagne@gmail.com>
2018-06-14 16:29:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,13 +2,17 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is __"
+    "My name is Mia."
   end
   get '/hometown' do
-    "My hometown is __"
+    "My hometown is Aurora, Colorado."
   end
   get '/favorite-song' do
-    "My favorite song is __"
+    "My favorite song is This Is America."
+  end
+  get '/favorite-food' do
+    "My favorite food is pasta."
   end
   
 end
+
